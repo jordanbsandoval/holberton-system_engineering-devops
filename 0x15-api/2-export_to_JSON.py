19c006f0250d543d428a545a01f65b569ab2877b
jordanbsandoval <1608@holbertonschool.com>
2021-05-04 01:08:06 -0400

    export_json_obj

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Python script to export data in the JSON format.
+"""
+
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    if len(argv) == 2:
+        url_base = 'https://jsonplaceholder.typicode.com/users/'
+        person_url = '{}{}'.format(url_base, argv[1])
+        todos_url = '{}{}/{}'.format(url_base, argv[1], 'todos')
+
+        """ do two request
+        one for user personal info and for todos tasks
+        """
+        person_res = requests.get(person_url)
+        todos_res = requests.get(todos_url)
+
+        """ get the obj responses body"""
+        person_obj = person_res.json()
+        todos_obj = todos_res.json()
+
+        """ working with the data """
+        username = person_obj['username']
+        result = []
+        filename = '{}.json'.format(person_obj['id'])
+
+        for obj in todos_obj:
+            task = {}
+            task['task'] = obj['title']
+            task['completed'] = obj['completed']
+            task['username'] = username
+            result.append(task)
+
+        with open(filename, 'w') as p:
+            json.dump({person_obj['id']: result}, p)
+
