0b9dd6b9d1c7e8abc05a9db6c2a90251d5e32e24
jordanbsandoval <1608@holbertonschool.com>
2021-05-04 00:36:47 -0400

    int_2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python3
-"""gather_data_from_an_API  This module request data from employee API"""
-
+"""
+gather_data_from_an_API  This module request data from employee API
+"""
+import json
 import requests
 import sys
 
-
-
-# Process functions
-def make_request(id):
-    """make request"""
-    user_r = "https://jsonplaceholder.typicode.com/users/" + id
-    todo_r = "https://jsonplaceholder.typicode.com/todos/?userId=" + id
-    dato = [requests.get(user_r).json(), requests.get(todo_r).json()]
-    return(data)
-
-
-def print_data(data):
-    """print data json"""
-    user_name = data[0]["name"]
-    tarea_completa = 0
-    task = 0
-
-    for i in data[1]:
-        task += 1
-        if i['completed']:
-            tarea_completa += 1
-
-    print("Employee {} is done with tasks({}/{}):".format(user_name,
-                                                          completed_task,
-                                                          task))
-
-    for item in data[1]:
-        if item['completed'] is True:
-            print('\t {}'.format(item['title']))
-
-
-def main():
-    if len(sys.argv) == 2:
-        dato = make_request(sys.argv[1])
-        print_data(dato)
-
-
 if __name__ == "__main__":
-    main()
+
+    if len(argv) == 2:
+        url_base = 'https://jsonplaceholder.typicode.com/users/'
+        person_url = '{}{}'.format(url_base, argv[1])
+        todos_url = '{}{}/{}'.format(url_base, argv[1], 'todos')
+
+        """ do two request
+        one for user personal info and for todos tasks
+        """
+        person_res = requests.get(person_url)
+        todos_res = requests.get(todos_url)
+
+        """ get the obj responses body"""
+        person_obj = person_res.json()
+        todos_obj = todos_res.json()
+
+        """ working with the data """
+        done_count = 0
+        done_tasks = []
+
+        for obj in todos_obj:
+            if obj['completed'] is True:
+                done_count += 1
+                done_tasks.append(obj['title'])
+
+        st = 'Employee {} is done with tasks({}/{}):'
+        print(st.format(person_obj['name'], done_count, len(todos_obj)))
+        for task in done_tasks:
+            print('\t {}'.format(task))
